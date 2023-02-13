14ef94cb31cc0cddef756510a0b2e29698b4015b
dike-okayama <lawliet.d.k.m@gmail.com>
2023-02-14 00:13:37 +0900

    Relaxed request frequency

--- AnnotationsRemover/static/script.js
+++ AnnotationsRemover/static/script.js
@@ -1,9 +1,15 @@
 var socket = io();
+var lastExecution = null;
 
 socket.on("text_update", function (json) {
     $(".output-box").val(json.source);
 });
 
 $("textarea").on("input", function () {
-    socket.emit("remove", { source: $(".input-box").val() });
+    if (lastExecution !== null) {
+        window.clearTimeout(lastExecution);
+    }
+    lastExecution = window.setTimeout(function () {
+        socket.emit("remove", { source: $(".input-box").val() });
+    }, 500);
 });
